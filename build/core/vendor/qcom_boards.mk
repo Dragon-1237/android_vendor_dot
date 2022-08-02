51264990da203144d5bd111d682af6bdf1c21422
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-08-02 20:40:03 +0530

    Update qcom_boards.mk

--- build/core/vendor/qcom_boards.mk
+++ build/core/vendor/qcom_boards.mk
@@ -2,15 +2,15 @@
 # TARGET_BOARD_PLATFORM specific featurization
 
 # Platform name variables - used in makefiles everywhere
-KONA := kona #SM8250
-LITO := lito #SM7250
-BENGAL := bengal #SM6115
-MSMNILE := msmnile #SM8150
-MSMSTEPPE := sm6150
-TRINKET := trinket #SM6125
-ATOLL := atoll #SM6250
-LAHAINA := lahaina #SM8350
-HOLI := holi #SM4350
+KONA ?= kona #SM8250
+LITO ?= lito #SM7250
+BENGAL ?= bengal #SM6115
+MSMNILE ?= msmnile #SM8150
+MSMSTEPPE ?= sm6150
+TRINKET ?= trinket #SM6125
+ATOLL ?= atoll #SM6250
+LAHAINA ?= lahaina #SM8350
+HOLI ?= holi #SM4350
 
 # A Family
 QCOM_BOARD_PLATFORMS += msm7x27a
